bbbbc04989471f8a3d45c1ea8e3e966534dd89fb
VazquezAlex <alejandrovazquez.dev@gmail.com>
2025-01-03 17:11:52 -0600

    feature: added model to pass data to view and display with thymeleaf

--- src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,13 +1,20 @@
 package com.alejandrovazquez.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
   @GetMapping("/details")
-  public String details() {
+  public String details(Model model) {
+    model.addAttribute("title", "Hola Mundo Spring Boot");
+    model.addAttribute("name", "Alejandro");
+    model.addAttribute("lastName", "Vazquez");
 
     return "details";
   }
