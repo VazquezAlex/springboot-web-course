44d8b9b07dc8450c7ad1d8a2a2c93974702bf81e
VazquezAlex <alejandrovazquez.dev@gmail.com>
2025-01-03 17:14:45 -0600

    feature: removed wrong exports

--- src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,10 +1,8 @@
 package com.alejandrovazquez.curso.springboot.webapp.springboot_web.controllers;
 
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
