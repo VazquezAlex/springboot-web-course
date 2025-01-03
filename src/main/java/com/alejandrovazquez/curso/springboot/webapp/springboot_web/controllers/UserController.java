4e7673c7675b92bce8cf2e203edeb19d975412c1
VazquezAlex <alejandrovazquez.dev@gmail.com>
2025-01-03 17:01:43 -0600

    feature: added mapping for details view

---
+++ src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.alejandrovazquez.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+  @GetMapping("/details")
+  public String details() {
+
+    return "details";
+  }
+
+}
