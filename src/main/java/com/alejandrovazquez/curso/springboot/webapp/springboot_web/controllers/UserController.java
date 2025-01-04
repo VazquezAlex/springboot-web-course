849909ae94acf3af528e64b62245f02c4eeccf50
VazquezAlex <alejandrovazquez.dev@gmail.com>
2025-01-03 23:53:51 -0600

    feature: added User model and implemented on view and endpoint

--- src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -5,14 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.alejandrovazquez.curso.springboot.webapp.springboot_web.models.User;
+
 @Controller
 public class UserController {
 
   @GetMapping("/details")
   public String details(Model model) {
+    User user = new User("Alejandro", "Vazquez");
+
     model.addAttribute("title", "Hola Mundo Spring Boot");
-    model.addAttribute("name", "Alejandro");
-    model.addAttribute("lastName", "Vazquez");
+    model.addAttribute("user", user);
 
     return "details";
   }
--- src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -7,16 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alejandrovazquez.curso.springboot.webapp.springboot_web.models.User;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
   @GetMapping("/user")
   public Map<String, Object> details() {
+
+    User user = new User("Alejandro", "Vazquez");
     Map<String, Object> body = new HashMap<String, Object>();
+
     body.put("title", "Hola Mundo Spring Boot");
-    body.put("name", "Alejandro");
-    body.put("lastName", "Vazquez");
+    body.put("user", user);
 
     return body;
   }
