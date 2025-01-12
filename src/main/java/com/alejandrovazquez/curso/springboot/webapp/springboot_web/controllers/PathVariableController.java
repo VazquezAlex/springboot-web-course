415469437e0e649b812cc3dd2798fe31cf1c202e
VazquezAlex <alejandrovazquez.dev@gmail.com>
2025-01-12 12:24:10 -0600

    feature: added dynamic path endpoint

---
+++ src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -0,0 +1,22 @@
+package com.alejandrovazquez.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alejandrovazquez.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+
+  @GetMapping("/baz/{message}")
+  public ParamDto baz(@PathVariable String message) {
+    ParamDto param = new ParamDto();
+    param.setMessage(message);
+
+    return param;
+  }
+
+}
