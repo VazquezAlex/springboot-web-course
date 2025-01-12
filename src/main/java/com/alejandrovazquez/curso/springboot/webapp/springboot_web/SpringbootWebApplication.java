7128f081d7a000347820ff7e49b111fec3e4ff90
VazquezAlex <alejandrovazquez.dev@gmail.com>
2025-01-12 13:02:03 -0600

    feature: added class config, added object for multiple property files, added encoding to allow special characters on property file

--- src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/alejandrovazquez/curso/springboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -2,10 +2,8 @@ package com.alejandrovazquez.curso.springboot.webapp.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:values.properties")
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
